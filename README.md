bbc7c7247b6e80443c3b90de4ff571fafe46d323
Marouene A <50542877+maroucode@users.noreply.github.com>
2020-11-22 00:08:36 +0100

    renaming project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TrainingProject
+# tasty
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.2.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'training-project'`, () => {
+  it(`should have as title 'tasty'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
    });
@@ -29,6 +29,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('training-project app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tasty app is running!');
   });
 });
